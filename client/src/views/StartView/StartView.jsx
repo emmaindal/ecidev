b2cd2a1a57739140bcfe1116d7e9ba53f9b572e5
emmaindal <emma.indahl@gmail.com>
2018-11-09 13:53:37 +0100

    temporary for deploy

--- client/src/views/StartView/StartView.jsx
+++ client/src/views/StartView/StartView.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Main from "./components/Main";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Main from "./components/Main";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
 
 const StartView = ({ handleSayHello }) => {
   return (
-    <div>
-      <Navbar handleSayHello={handleSayHello} />
-      <Header />
-      <Main />
-      <Contact />
-      <Footer />
-    </div>
+    // <Navbar handleSayHello={handleSayHello} />
+    <Header />
+    // <Main />
+    // <Contact />
+    // <Footer />
   );
 };
 
